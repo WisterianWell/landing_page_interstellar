4718ed66b26b2cc5f7ae12b7fba5aa63a7286190
Lucas Tanino <lucastanino@live.com>
2024-04-15 18:48:42 -0300

    adicionado elementos finais

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(button) {
+            const targetTab = button.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
+            hideAllTabs();
+            tab.classList.add('cast__list--is-active');
+            removeActiveButton();
+            button.target.classList.add('cast__nav__button--is-active')
+        })
+    }
+})
+
+function removeActiveButton() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('cast__nav__button--is-active');
+    }
+}
+
+function hideAllTabs() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('cast__list--is-active');
+    }
+}
